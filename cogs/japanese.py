20af5ffbc909de63ef3140bf668bcf3dd8e017be
Christina Hanson <christinahansondesu@gmail.com>
2024-09-17 07:55:39 +1200

    a

--- cogs/japanese.py
+++ cogs/japanese.py
@@ -129,7 +129,7 @@ class Japanese(commands.Cog):
                 {"role": "system", "content": prompt},
                 *messages,
             ],
-            max_tokens=512,
+            max_tokens=4096,
         )
         result = completion.choices[0].message.content
 
